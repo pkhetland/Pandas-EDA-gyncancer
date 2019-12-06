efe49a72ec18925ffa1ca9f74d6b0140ebda0a39
Petter <petter@Hetland-Air.local>
2019-12-06 19:25:45 +0100

    Changed app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
 
 ''' Defining main dataset and making early adjustments
 '''
